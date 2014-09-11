42bc45584a7bc40ea25a502888ac1efda65dd00f
Lukas Matt <lukas@zauberstuhl.de>
2014-09-12 00:48:43 +0200

    Moved chat from server section

--- lib/vines/config/diaspora.rb
+++ lib/vines/config/diaspora.rb
@@ -3,30 +3,30 @@
 # ############################################################## #
 
 Vines::Config.configure do
-  log AppConfig.server.chat.server.log.file.to_s do
-    level AppConfig.server.chat.server.log.level.to_sym
+  log AppConfig.chat.server.log.file.to_s do
+    level AppConfig.chat.server.log.level.to_sym
   end
 
-  certs AppConfig.server.chat.server.certs.to_s
+  certs AppConfig.chat.server.certs.to_s
 
   host diaspora_domain do
     cross_domain_messages true
     storage 'sql'
   end
 
-  client AppConfig.server.chat.server.c2s.address.to_s, AppConfig.server.chat.server.c2s.port.to_i do
-    max_stanza_size AppConfig.server.chat.server.c2s.max_stanza_size.to_i
-    max_resources_per_account AppConfig.server.chat.server.c2s.max_resources_per_account.to_i
+  client AppConfig.chat.server.c2s.address.to_s, AppConfig.chat.server.c2s.port.to_i do
+    max_stanza_size AppConfig.chat.server.c2s.max_stanza_size.to_i
+    max_resources_per_account AppConfig.chat.server.c2s.max_resources_per_account.to_i
   end
 
-  server AppConfig.server.chat.server.s2s.address.to_s, AppConfig.server.chat.server.s2s.port.to_i do
-    max_stanza_size AppConfig.server.chat.server.s2s.max_stanza_size.to_i
+  server AppConfig.chat.server.s2s.address.to_s, AppConfig.chat.server.s2s.port.to_i do
+    max_stanza_size AppConfig.chat.server.s2s.max_stanza_size.to_i
   end
 
-  http AppConfig.server.chat.server.bosh.address.to_s, AppConfig.server.chat.server.bosh.port.to_i do
-    bind AppConfig.server.chat.server.bosh.bind.to_s
-    max_stanza_size AppConfig.server.chat.server.bosh.max_stanza_size.to_i
-    max_resources_per_account AppConfig.server.chat.server.bosh.max_resources_per_account.to_i
+  http AppConfig.chat.server.bosh.address.to_s, AppConfig.chat.server.bosh.port.to_i do
+    bind AppConfig.chat.server.bosh.bind.to_s
+    max_stanza_size AppConfig.chat.server.bosh.max_stanza_size.to_i
+    max_resources_per_account AppConfig.chat.server.bosh.max_resources_per_account.to_i
     root 'public'
     vroute ''
   end
